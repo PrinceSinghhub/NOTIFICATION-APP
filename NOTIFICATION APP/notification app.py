04b7d9ec74f4802793afc5483c0c70bb9a308106
Prince Singh <thisisprincesingh@gmail.com>
2023-02-24 19:21:18 +0530

    Add files via upload
    add all files

---
+++ NOTIFICATION APP/notification app.py
@@ -0,0 +1,9 @@
+import time
+from plyer import notification
+if __name__=="__main__":
+    notification.notify(
+        title="CODEX CODER",
+        message="Plese drink water",
+        app_icon = "cod.ico",
+        timeout=15
+    )
